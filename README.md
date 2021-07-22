e7b1f66c5b8723342f722b142a5d680f1caffaf0
Samil Hazir <samil@algoritmik.net>
2021-07-22 17:13:49 +0100

    BG color option added

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # Image Resizer Service
 
-docker build . --tag image-resizer
+This is a simple image resizing docker container service with python 2
 
+## Parameters
+
+| Name  | Type | Desc |
+| ------------ | ------------  |------------ |
+| url  |    Optional* | Image URL.  |
+| url64  |    Optional* | Base 64 encoded Image URL.  |
+| bg  |    Optional*  | Background color Hex code  |
+| w  |    Optional  | Width (Default 960px)  |
+| h  |    Optional  | Height (Default 960px)  |
+
+###### One of the URL options or bg color is required, all other parameters are optional.
+
+------------
+
+## Buillding and running as a docker container
+
+```
+docker build . --tag image-resizer
 docker run --rm -i -t -p 8000:18080 image-resizer
+```
--- image_resizer.py
+++ image_resizer.py
@@ -4,28 +4,14 @@ from PIL import Image
 from flask import (Flask, request, send_file)
 import urllib
 import cStringIO
-import StringIO 
+import StringIO
 import re
 
 app = Flask(__name__)
 port = int(os.getenv("PORT", 18080))
-defaultSizes = 1920, 1920
+defaultSizes = 960, 960
 
-def getFileExt(url):
-    rv = None
-    m = re.match('^.+\.([^.]+)$', url)
-    if m:
-        rv = m.group(1)
-    return rv
-
-def getFileName(url):
-    rv = None
-    m = re.match('^.+/(.+?)\.[A-Za-z]{3,4}$', url)
-    if m:
-        rv = m.group(1)
-    return rv
-
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def thumb():
     w = request.args.get('w')
     h = request.args.get('h')
@@ -34,22 +20,30 @@ def thumb():
 
     if h is None:
         h = defaultSizes[1]
-    
+
     size = int(w), int(h)
 
-    urlBase64 = request.args.get('urlBase64')
-    if urlBase64:
-        url = base64.b64decode(urlBase64)
+    url64 = request.args.get('url64')
+    if url64:
+        url = base64.b64decode(url64)
     else:
         url = request.args.get('url')
 
-    if not url:
-        return "Homepage"
-
     print("Processing URL \"%s\" now ..." % url)
-    file = cStringIO.StringIO(urllib.urlopen(url).read())
-    img = Image.open(file)
-    img.thumbnail(size)
+
+    if url:
+        file = cStringIO.StringIO(urllib.urlopen(url).read())
+        img = Image.open(file)
+        img.thumbnail(size)
+    else:
+        bg = request.args.get('bg')
+        if not bg:
+            bg = 0
+        if len(bg) != 3 and len(bg) != 6:
+            return "BAD REQUEST", 400
+        
+        img = Image.new("RGB", size, '#' + bg)
+
     img_io = StringIO.StringIO()
     img.save(img_io, 'JPEG', quality=70)
     img_io.seek(0)
